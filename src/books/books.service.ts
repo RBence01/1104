290474376e380c93fdc2fce5292762039f6e9cd7
RBence01 <rabel.bence15@gmail.com>
2024-11-06 14:28:43 +0100

    cuccok

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -2,10 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 import { Book } from './entities/book.entity';
+import { createPool, Pool } from 'mysql2/promise'
 
 @Injectable()
 export class BooksService {
+  connection: Pool;
   data: Book[] = [];
+  constructor() {
+    this.connection = createPool({
+      host: 'localhost',
+      user: 'root',
+      database: 'konyvtar'
+    });
+  }
+
   create(createBookDto: CreateBookDto) {
     try {
       let id: number = 0;
@@ -17,23 +27,29 @@ export class BooksService {
     }
   }
 
-  findAll() {
-    return this.data;
+  async findAll() {
+    const [data] = await this.connection.query('SELECT * FROM books');
+    return data;
   }
 
   findOne(id: number) {
+    //this.connection.query('SELECT * FROM books WHERE id === ?', id);
     const index = this.data.findIndex(e => e.id === id);
     if (index === -1) return null;
     const book: any = this.data[index];
-    if (this.data.length -1 > index) book.nextId = this.data[index+1].id;
+    if (this.data.length - 1 > index) book.nextId = this.data[index + 1].id;
     else book.nextId = null;
     return book;
   }
 
-  update(id: number, updateBookDto: UpdateBookDto) {
-    const book = this.data.find(e => e.id === id);
-    if (!book) return false;
-    book.update(updateBookDto);
+  async update(id: number, updateBookDto: UpdateBookDto) {
+    const [data] = await this.connection.query('SELECT * FROM books WHERE id = ?', [id]);
+    const books: Book[] = data as Book[];
+    if (books.length != 1) return false;
+    const newBook = {...books[0], ...updateBookDto}
+    await this.connection.query('UPDATE SET title = ?, author = ?, isbn = ?, publishYear = ?, reserved = ? WHERE id = ?',
+      [newBook.title, newBook.author, newBook.isbn, newBook.publishYear, newBook.reserved, newBook.id]
+    )
     return true;
   }
 
--- src/books/dto/update-book.dto.ts
+++ src/books/dto/update-book.dto.ts
@@ -1,9 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookDto } from './create-book.dto';
-import { IsBoolean, IsNotEmpty } from 'class-validator';
+import { IsBoolean, IsOptional } from 'class-validator';
 
 export class UpdateBookDto extends PartialType(CreateBookDto) {
-  @IsNotEmpty()
+  @IsOptional()
   @IsBoolean()
   reserved?: boolean;
 }
